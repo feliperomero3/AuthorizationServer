2be43fd0a9e05f155481cffa7cb04c20ef82b5a8
Felipe Romero <feliperomero3@outlook.com>
2022-03-09 21:28:09 -0600

    Adding a client that uses OAuth’s client credentials grant type.

---
+++ src/Clients.cs
@@ -0,0 +1,23 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace AuthorizationServer
+{
+    internal class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "oauthClient",
+                    ClientName = "Example client application using client credentials.",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = new List<Secret> {new Secret("SuperSecretPassword".Sha256())},
+                    AllowedScopes = new List<string> {"api1.read"}
+                }
+            };
+        }
+    }
+}
