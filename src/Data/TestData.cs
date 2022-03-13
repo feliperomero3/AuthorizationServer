ef849ad6f3c03175a8b966a842d50fbc3ccc41d0
Felipe Romero <feliperomero3@outlook.com>
2022-03-13 01:04:56 -0600

    Running the IdentityServer EF Core migrations.

---
+++ src/Data/TestData.cs
@@ -0,0 +1,54 @@
+﻿using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using System;
+using System.Linq;
+
+namespace AuthorizationServer.Data
+{
+    internal class TestData
+    {
+        internal static void Seed(PersistedGrantDbContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void Seed(ConfigurationDbContext context)
+        {
+            if (!context.Clients.Any())
+            {
+                foreach (var client in Clients.Get())
+                {
+                    context.Clients.Add(client.ToEntity());
+                }
+                context.SaveChanges();
+            }
+
+            if (!context.IdentityResources.Any())
+            {
+                foreach (var resource in Resources.GetIdentityResources())
+                {
+                    context.IdentityResources.Add(resource.ToEntity());
+                }
+                context.SaveChanges();
+            }
+
+            if (!context.ApiScopes.Any())
+            {
+                foreach (var scope in Resources.GetApiScopes())
+                {
+                    context.ApiScopes.Add(scope.ToEntity());
+                }
+                context.SaveChanges();
+            }
+
+            if (!context.ApiResources.Any())
+            {
+                foreach (var resource in Resources.GetApiResources())
+                {
+                    context.ApiResources.Add(resource.ToEntity());
+                }
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,13 +1,47 @@
+using AuthorizationServer.Data;
+using IdentityServer4.EntityFramework.DbContexts;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace AuthorizationServer
 {
-    public static class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var grantsDb = services.GetRequiredService<PersistedGrantDbContext>();
+
+                    grantsDb.Database.Migrate();
+
+                    var configurationDb = services.GetRequiredService<ConfigurationDbContext>();
+
+                    configurationDb.Database.Migrate();
+
+                    TestData.Seed(configurationDb);
+                }
+                catch (SqlException e)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+
+                    logger.LogError(e, "An error occurred creating the Database.");
+
+                    throw;
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
