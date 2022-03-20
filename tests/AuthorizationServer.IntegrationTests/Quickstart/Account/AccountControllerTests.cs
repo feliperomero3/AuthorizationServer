2267c5e3c6fe00a986c6c6aabead572a94afaaf9
Felipe Romero <feliperomero3@outlook.com>
2022-03-20 01:04:59 -0600

    Add account controller integration tests.

---
+++ tests/AuthorizationServer.IntegrationTests/Quickstart/Account/AccountControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using MoneySmart.IntegrationTests;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AuthorizationServer.IntegrationTests
+{
+    public class AccountControllerTests : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly CustomWebApplicationFactory<Startup> _factory;
+
+        public AccountControllerTests(CustomWebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+            _factory.ClientOptions.AllowAutoRedirect = false;
+            _factory.ClientOptions.BaseAddress = new Uri("https://localhost:5000/Account/");
+        }
+
+        [Fact]
+        public async Task Get_Login_Page_Returns_SuccessAsync()
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync("Login");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Get_Logout_Page_Returns_SuccessAsync()
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync("Logout");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
